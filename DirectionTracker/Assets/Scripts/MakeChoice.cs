973ca81dc1e414970495d967f97ad52f33821f07
Bexasusual <3046853673@qq.com>
2019-08-30 14:44:58 +0800

    upload source code of DirectionTracker Ver1.0

---
+++ DirectionTracker/Assets/Scripts/MakeChoice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeChoice : MonoBehaviour
+{
+    public GameObject AnswerCanvas;
+
+    public int id;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClick() //Remember to set it public
+    {
+        AnswerCanvas.SendMessage("GetAnswer", id);
+    }
+}
---
+++ DirectionTracker/Assets/Scripts/StartController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+    public GameObject startButton;
+    public GameObject helpButton;
+    public GameObject exitButton;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ShowHelp()
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
